8a9ed4a8c1b4c3ff6ac655d929e78802ef31738a
waltersimekha <waltersimekha@gmail.com>
2024-03-12 14:50:33 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "a") as file:
+        file.write("\n" + append_content)
 
 def read_file(file_name):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "r") as file:
+        content = file.read()
+    return content
